b1212f591121e9e2b26e654d752c7af2bd4d1bda
test@test.com <test@test.com>
2022-12-27 23:12:14 +0900

    DAO 에러 해결(class>interface)

--- src/main/java/com/example/lesson04/dao/UserDAO.java
+++ src/main/java/com/example/lesson04/dao/UserDAO.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserDAO {
+public interface UserDAO {
 
 	public void insertUser(
 			@Param("name") String name
